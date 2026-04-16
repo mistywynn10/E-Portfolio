60f5032eedd313334bb5a90b48470bd5a777af39
mistywynn10 <mistywynn10@gmail.com>
2026-04-16 13:00:13 -0400

    Contact

---
+++ index.js
@@ -0,0 +1,25 @@
+// template_og0k0d3
+// service_ytt0oop
+// user_WOscBdVwyZ4MhyaPW
+
+function contact() {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+           'service_ytt0oop', 
+           'template_og0k0d3',
+           event.target,
+           'WOscBdVwyZ4MhyaPW'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable.  Please contact me directly at mistywynn10@gmail.com."
+            )
+        })
+}
